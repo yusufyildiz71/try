d5328e8219e2ff7a60732d5bcc79b75b8db1316a
Taha Yusuf Yıldız <tahayusufyildiz@Taha-Mac-mini.local>
2022-04-12 09:37:36 +0300

    first

---
+++ android/app/src/main/kotlin/com/example/try_tidyes/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.try_tidyes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+void main()=>runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "tidyes",
+      home:Iskele(),
+    );
+  }
+}
+class Iskele extends StatelessWidget {
+  const Iskele({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 53, 59, 53),
+      body: MainPage(),
+    );
+  }
+}
+class MainPage extends StatefulWidget {
+  const MainPage({ Key? key }) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+      height: 130.0,
+      
+      child: Center(child: Row(
+        children: <Widget>[
+         
+          Row(
+            children: <Widget>[
+              Text("selam")
+              
+            ],
+          )
+        ],
+      )),
+    );
+  }
+}
