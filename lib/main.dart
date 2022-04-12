69352948a3d6038ec9670d34196d8cbe9a668396
Taha Yusuf Yıldız <tahayusufyildiz@Taha-Mac-mini.local>
2022-04-12 15:08:47 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -3,22 +3,23 @@ import 'dart:html';
 import 'package:flutter/material.dart';
 import 'package:try_tidyes/video.dart';
 
-void main()=>runApp(MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-       debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false,
       title: "tidyes",
-      home:Iskele(),
+      home: Iskele(),
     );
   }
 }
+
 class Iskele extends StatelessWidget {
-  const Iskele({ Key? key }) : super(key: key);
+  const Iskele({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -28,8 +29,9 @@ class Iskele extends StatelessWidget {
     );
   }
 }
+
 class MainPage extends StatefulWidget {
-  const MainPage({ Key? key }) : super(key: key);
+  const MainPage({Key? key}) : super(key: key);
 
   @override
   State<MainPage> createState() => _MainPageState();
@@ -39,15 +41,40 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.black,
+      color: Colors.white,
       height: 130.0,
-      
-      
-    child: SizedBox(
+      child: Row(
+        children: [
+          Row(
+            children: [
+              SizedBox(
+                child: Video(),
+                height: double.infinity,
+                width: 200,
+              ),
+              SizedBox(
+                height: double.infinity,
+                width: 880,
+                child: Column(children: [
+                  Text(
+                    "sorusorusorusorusorusorusorusorusorusorusorusorusorusorusorusorusorusorusorusorusorusorusorusorusorusorusorusorusorusoruso",
+                    style: DefaultTextStyle.of(context)
+                        .style
+                        .apply(fontSizeFactor: 2.0),
+                        textAlign: TextAlign.center,
+                  )
+                ]),
+              )
+            ],
+          )
+        ],
+      ),
+
+      /*child: SizedBox(
       child: Video(),
       height: double.infinity,
       width: 200,
-      )
+      )*/
     );
   }
 }
